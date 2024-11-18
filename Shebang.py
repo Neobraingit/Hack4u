c2644e94be580544009ba16e6ee669686e56f98f
neobraingit <neobrain@protonmail.com>
2024-11-18 19:02:57 +0100

    Shebang y convenciones

---
+++ Shebang.py
@@ -0,0 +1,8 @@
+#|/usr/bin/env python3
+
+# A continuación, podemos poner una función para tener el código más ordenado
+
+if __name__== '__main__':
+    print ('Soy el módulo principal')
+else:
+    print ('No soy el módulo principal')
