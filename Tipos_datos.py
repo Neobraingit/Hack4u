497b15a501cc07557715d9b8e265c230f92d5cc7
neobraingit <neobrain@protonmail.com>
2024-11-18 19:41:51 +0100

    Tipos de datos

---
+++ Tipos_datos.py
@@ -0,0 +1,23 @@
+
+#!/usr/bin/env python3
+
+# CADENAS O STRINGS
+
+cadena = 'mi_cadena' # Esta variable contiene una cadena
+ip_address = '192.168.1.145' # Aunque sean números, al ir entre comillas serán tratado como string
+
+'''_________________________________________________________'''
+
+# LISTAS
+
+# Las listas son contenedores que pueden guardar diferentes tipos de datos. Van con corchetes.
+
+lista = []
+
+# Con el método .append() añadiremos índices a la lista.
+# Podemos acceder a los índices de las listas con el formato []
+
+print (lista[1]) # En este caso, accedemos al índice 1, que comenzando a contar desde 0, será el segundo de la lista
+
+'''--------------------------------------------------------'''
+
