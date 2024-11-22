9572f269fe516f94c7ddfdc972362b648c1909d7
neobraingit <neobrain@protonmail.com>
2024-11-22 17:07:20 +0100

    String_formatting

---
+++ String_formatting.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+name = 'Marcos'
+
+'''---Manera simple----'''
+
+print ('Hola, mi nombre es: ' + name)  # Esta es una manera simple de representar la cadena
+
+'''---Porcentaje s----'''
+
+print ('Hola, mi nombre es: %s', name) # Esta es la manera con el porcentaje
+
+'''.format()'''
+
+print ('Hola, soy {}'.format(name))
+
+'''---f-strings----'''
+
+print (f'Hola, soy {name}')
